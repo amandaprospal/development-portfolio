83e433b7a8d8896721476ed5dedc686c1efcabf4
Amanda <macbook@Amandas-MacBook-Pro-2.local>
2018-12-31 22:30:14 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "amandaprospal.com/precache-manifest.6730d0b2076acc56e91e141b86d7da4a.js"
+  "/precache-manifest.bcb95f725209a6dd6119afcd845ff5ed.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("amandaprospal.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
