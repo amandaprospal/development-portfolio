12acc960694580a7190d4b15dc633bf22543e280
Amanda <macbook@Amandas-MacBook-Pro-2.local>
2018-12-31 22:11:00 -0500

    Updates

---
+++ precache-manifest.bcb95f725209a6dd6119afcd845ff5ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "295f202ade09d60c3cf9",
+    "url": "/static/css/main.1b86e1a6.chunk.css"
+  },
+  {
+    "revision": "295f202ade09d60c3cf9",
+    "url": "/static/js/main.295f202a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd97702b814925ffd39cf3b40ce1d861",
+    "url": "/index.html"
+  }
+];
